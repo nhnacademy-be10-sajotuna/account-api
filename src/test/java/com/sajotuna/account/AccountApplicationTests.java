a1d2bcad1224fdf7b65150ef00b4e16830fc29e5
flashdragon <yooh3574@naver.com>
2025-06-10 09:43:48 +0900

    [feat] account init

---
+++ src/test/java/com/sajotuna/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajotuna.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
